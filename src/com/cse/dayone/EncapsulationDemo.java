265b1187d646d31d461d662af01c63761160fed1
MSGayathiri <gayathirimani2003@gmail.com>
2024-07-30 20:56:18 +0530

    Encapsulation

---
+++ src/com/cse/dayone/EncapsulationDemo.java
@@ -0,0 +1,23 @@
+package com.cse.dayone;
+
+import com.cse.daythree.OopsConceptDemo;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		OopsConceptDemo obj = new OopsConceptDemo();
+				obj.setSerialNum(101);
+				obj.setName("Gayu");
+				obj.setAge(20);
+				System.out.println(obj +"age");
+				
+				
+	
+	
+	
+	}
+	
+	
+
+}
